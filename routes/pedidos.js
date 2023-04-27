22141e1fe047d6d19373a4b3a5d8c321efea1437
luucdamaso <lucas.s.damaso@hotmail.com>
2023-04-27 17:43:27 -0300

    Remoção de produtos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -48,3 +48,60 @@ router.get("/pedidos/clientes/:id", async (req, res) => {
 
 
 module.exports = router;
+
+// Post
+router.post("/pedidos", async (req, res) => {
+    try {
+      const pedidos = req.body;
+      const pedidosCriados = await Pedido.bulkCreate(pedidos);
+      res.status(201).json(pedidosCriados);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Um erro aconteceu" });
+    }
+  });
+// Put
+
+// Delete
+
+router.delete("/pedidos/:id", async (req, res) => {
+    try {
+        const pedido = await Pedido.findByPk(req.params.id);
+        if(!pedido){
+            return res.status(404).json({ message: "Produto não encontrado" });
+        }
+        await pedido.destroy();
+        res.json({ message: "Pedido removido." })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Um erro aconteceu." })
+    }
+});
+
+router.delete("/pedidos/clientes/:id", async (req, res) => {
+    try {
+        const pedido = await Pedido.findAll({ where: { clientesId: req.params.id } });
+        if(pedido.length === 0) {
+            return res.status(404).json({ message: "Pedidos não encontrados." });
+        }
+        await pedido.destroy({ where: { clientesId: req.params.id } });
+
+        res.json({ message: "Pedidos removidos com sucesso." })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Um erro aconteceu." })
+    }
+});
+
+router.delete("/pedidos/produtos/:id", async (req, res) => {
+    try {
+        const pedido = await Pedido.findAll({ where: { produtosId: req.params.id } });
+        if(pedido.length === 0) {
+            return res.status(404).json({ message: "Pedidos não encontrados." });
+        }
+        await pedido.destroy({ where: { produtosId: req.params.id } });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+});
