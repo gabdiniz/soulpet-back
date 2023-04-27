32f36ea94b8e9a13b9925f9f06ff7782a71ecdb3
Mikael Meira <mikaelmenardi@gmail.com>
2023-04-27 17:48:16 -0300

    BUSCAR SERVIÇOS GET

--- routes/servico.js
+++ routes/servico.js
@@ -1,20 +1,7 @@
-const { Router } = require("express");
-const Servico = require("../database/servico")
+const { Router } = require('express');
+const Servico = require('../database/servico');
 
 const router = Router();
-
-router.post("/servicos", async(req, res) => {
-  const { nome, preco } = req.body;
-  if (!nome) return res.status(400).json({ message: "O nome é obrigatório"});
-  if (!preco) return res.status(400).json({ message: "O preço é obrigatório"});
-  try {
-    const servico = await Servico.create({ nome, preco });
-    res.status(201).json(servico)
-  }
-  catch (e) {
-    res.status(500).json({ message: "Um erro aconteceu!" })
-  }
-});
 
 // Rota para listar todos os serviços
 router.get('/servicos', async (req, res) => {
@@ -45,5 +32,6 @@ router.get('/servicos/:id', async (req, res) => {
     res.status(500).send('Erro ao buscar serviço');
   }
 });
+
 
 module.exports = router;
