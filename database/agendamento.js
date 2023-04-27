c9957ff97307d6512a863798ddd154422625edbd
TamiresDellaRovere <tamires.rovere@hotmail.com>
2023-04-27 17:14:45 -0300

    criação do model Agendamento

---
+++ database/agendamento.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const Pet = require("./pet");
+const Servico = require("./servico");
+
+const Agendamento = connection.define("agendamento", {
+    petId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Pet,
+            key: 'id'
+        }
+    },
+    servicoId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Servico,
+            key: 'id'
+        }
+    },
+    dataAgendada: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    realizada: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+});
+
+Pet.belongsToMany(Servico, { through: Agendamento});
+Servico.belongsToMany(Pet, { through: Agendamento});
