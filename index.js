a7f090b94c46c917f85d20c2671f13e001e03e80
luucdamaso <lucas.s.damaso@hotmail.com>
2023-04-28 09:46:28 -0300

    Crie uma rota POST para inserir múltiplos pedidos.

--- index.js
+++ index.js
@@ -21,12 +21,14 @@ const rotasClientes = require("./routes/clientes");
 const rotasPets = require("./routes/pets");
 const rotasProdutos = require("./routes/produtos");
 const rotasServico = require("./routes/servicos");
+const rotasPedidos = require("./routes/pedidos")
 
 // Juntar ao app as rotas dos arquivos
 app.use(rotasClientes); // Configurar o grupo de rotas no app
 app.use(rotasPets);
 app.use(rotasProdutos);
 app.use(rotasServico);
+app.use(rotasPedidos);
 
 // Escuta de eventos (listen)
 app.listen(3001, () => {
