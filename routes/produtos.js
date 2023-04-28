161247fcce3d58f70ad573624b3f1f5ed8da8ba6
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-28 17:48:38 -0300

    Fix produtos.js

--- routes/produtos.js
+++ routes/produtos.js
@@ -42,55 +42,55 @@ router.get("/produtos/:id", async (req, res) => {
 });
 
 router.post("/produtos", async (req, res) => {
-  
+
     const { nome, preco, descricao, desconto, dataDesconto, categoria } = req.body;
     const data = toDate(dataDesconto, 'yyyy-MM-dd', { timeZone: 'America/Sao_Paulo' });
     const dataAtual = toDate(new Date(), { timeZone: 'America/Sao_Paulo' });
     dataAtual.setHours(0, 0, 0, 0);
 
-if (categoria !== "higiene" && categoria !== "brinquedos" && categoria !== "conforto" ) {
-        return res.status(400).json({ message: "Adicione uma categoria válida: higiene, brinquedos ou conforto." }); 
+    if (categoria !== "higiene" && categoria !== "brinquedos" && categoria !== "conforto") {
+        return res.status(400).json({ message: "Adicione uma categoria válida: higiene, brinquedos ou conforto." });
     }
-    if(data < dataAtual) {
-        return res.status(400).json({ message: "A data não pode ser anterior a data atual." }); 
+    if (data < dataAtual) {
+        return res.status(400).json({ message: "A data não pode ser anterior a data atual." });
     }
-    if(desconto < 0 || desconto > 100) {
-        return res.status(400).json({ message: "Aplique um desconto de 0 a 100%." }); 
+    if (desconto < 0 || desconto > 100) {
+        return res.status(400).json({ message: "Aplique um desconto de 0 a 100%." });
     }
     try {
         const novo = await Produto.create(
             { nome, preco, descricao, desconto, dataDesconto, categoria },
         );
-            res.status(201).json(novo);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: "Um erro aconteceu." });
-        }
-    });
+        res.status(201).json(novo);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+});
 
 router.delete("/produtos/all", async (req, res) => {
-    try{
-        await Produto.destroy({where: {}});
-        res.json({message: "Todos os produtos foram deletados!"})
-    } catch(err){
+    try {
+        await Produto.destroy({ where: {} });
+        res.json({ message: "Todos os produtos foram deletados!" })
+    } catch (err) {
         console.log(err);
-        res.status(500).json({message:"Um erro aconteceu!"})
+        res.status(500).json({ message: "Um erro aconteceu!" })
     }
 });
 
 
-router.delete("/produtos/:id", async (req,res) => {
+router.delete("/produtos/:id", async (req, res) => {
     const produto = await Produto.findByPk(req.params.id);
-    try{
+    try {
         if (produto) {
             await Produto.destroy({ where: { id: req.params.id } })
-            res.json({message:"Produto removido com sucesso!"})
+            res.json({ message: "Produto removido com sucesso!" })
         } else {
-            res.status(404).json({message:"Produto não encontrado!"})
+            res.status(404).json({ message: "Produto não encontrado!" })
         }
-    } catch (err){
+    } catch (err) {
         console.log(err);
-        res.status(500).json({message:"Um erro aconteceu!"});
+        res.status(500).json({ message: "Um erro aconteceu!" });
     }
 });
 
@@ -98,82 +98,37 @@ router.put("/produtos/:id", async (req, res) => {
 
     const { nome, preco, descricao, desconto, dataDesconto, categoria } = req.body;
     const { id } = req.params;
-    
-    try {
-    const produto = await Produto.findOne({ where: { id } });
-    const data = toDate(dataDesconto, 'yyyy-MM-dd', { timeZone: 'America/Sao_Paulo' });
-    const dataAtual = toDate(new Date(), { timeZone: 'America/Sao_Paulo' });
-    dataAtual.setHours(0, 0, 0, 0);
 
-    if (produto) {
-        if (categoria === "higiene" || categoria === "brinquedos" || categoria === "conforto" ) {
-            
-        } else {
-        return res.status(400).json({ message: "Adicione uma categoria válida: higiene, brinquedos ou conforto." }); 
-        } 
-        if(data < dataAtual || data > dataDesconto) {
-            return res.status(400).json({ message: "A data não pode ser anterior a data atual." }); 
-        }
-        if(desconto < 0 || desconto > 100) {
-            return res.status(400).json({ message: "Aplique um desconto de 0 a 100% ." }); 
-        }
-        await Produto.update(
-            { nome, preco, descricao, desconto, dataDesconto, categoria },
-            { where: { id: req.params.id } } 
+    try {
+        const produto = await Produto.findOne({ where: { id } });
+        const data = toDate(dataDesconto, 'yyyy-MM-dd', { timeZone: 'America/Sao_Paulo' });
+        const dataAtual = toDate(new Date(), { timeZone: 'America/Sao_Paulo' });
+        dataAtual.setHours(0, 0, 0, 0);
+
+        if (produto) {
+            if (categoria === "higiene" || categoria === "brinquedos" || categoria === "conforto") {
+
+            } else {
+                return res.status(400).json({ message: "Adicione uma categoria válida: higiene, brinquedos ou conforto." });
+            }
+            if (data < dataAtual || data > dataDesconto) {
+                return res.status(400).json({ message: "A data não pode ser anterior a data atual." });
+            }
+            if (desconto < 0 || desconto > 100) {
+                return res.status(400).json({ message: "Aplique um desconto de 0 a 100% ." });
+            }
+            await Produto.update(
+                { nome, preco, descricao, desconto, dataDesconto, categoria },
+                { where: { id: req.params.id } }
             );
             res.json({ message: "O produto foi editado." });
-    } else {
-        res.status(404).json({ message: "Produto não encontrado." });
-    }
-    } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Um erro aconteceu." });
-    }
-});
-
-// Delete
-
-router.delete("/pedidos/:id", async (req, res) => {
-    try {
-        const pedido = await Pedido.findByPk(req.params.id);
-        if(!pedido){
-            return res.status(404).json({ message: "Produto não encontrado" });
+        } else {
+            res.status(404).json({ message: "Produto não encontrado." });
         }
-        await pedido.destroy();
-        res.json({ message: "Pedido removido." })
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Um erro aconteceu." })
-    }
-});
-
-router.delete("/pedidos/clientes/:id", async (req, res) => {
-    try {
-        const pedido = await Pedido.findAll({ where: { clientesId: req.params.id } });
-        if(pedido.length === 0) {
-            return res.status(404).json({ message: "Pedidos não encontrados." });
-        }
-        await pedido.destroy({ where: { clientesId: req.params.id } });
-
-        res.json({ message: "Pedidos removidos com sucesso." })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Um erro aconteceu." })
-    }
-});
-
-router.delete("/pedidos/produtos/:id", async (req, res) => {
-    try {
-        const pedido = await Pedido.findAll({ where: { produtosId: req.params.id } });
-        if(pedido.length === 0) {
-            return res.status(404).json({ message: "Pedidos não encontrados." });
-        }
-        await pedido.destroy({ where: { produtosId: req.params.id } });
-    } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(500).json({ message: "Um erro aconteceu." });
     }
 });
-
 
 module.exports = router;
